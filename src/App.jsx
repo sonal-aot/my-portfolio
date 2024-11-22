3b4b79a971d8310b6fa124ad1bc1b84ed2026422
sonal-aot <sonal.sai@aot-technologies.com>
2024-11-22 09:02:29 -0800

    "Added mainContainer class to App component and imported Profile component"

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './components/Header/Header'
+import Profile from './components/Profile/Profile'
 
 function App() {
 
   return (
-    <>
+    <div className='mainContainer'>
       <Header/>
-    </>
+      <Profile/>
+    </div>
   )
 }
 
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import '../../styles/components/Profile/Profile.scss';
+import profileImage from "../../assets/images/profile.jpg";
+
+const Profile = () => {
+  return (
+    <section className="profile">
+      <div className="profileContent">
+        <div className="profileImageContainer">
+          <img src={profileImage} alt="Sonal Sai" className="profileImage" />
+        </div>
+        <div className="profileText">
+          <h1 className="profileTitle">Hi, I'm Sonal Sai</h1>
+          <p className="profileSubtitle">A Fresher Software Developer</p>
+          <p className="profileDescription">
+            Passionate about building innovative solutions with modern technologies. Let's work together to create something amazing!
+          </p>
+          <a href="#contact" className="ctaButton">Contact Me</a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Profile;
